0a08eff77c74d474950b1f16bd2bbe8a6d870bb0
Om Somani <omsomani789@gmail.com>
2025-11-05 22:19:10 +0530

    updated ui

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -31,7 +31,7 @@ export function LoginForm({ userType, onSuccess }: LoginFormProps) {
                 onSuccess();
             } else {
                 setError(userType === 'customer'
-                    ? 'Invalid phone number. Use demo numbers: +919157992385'
+                    ? 'Invalid phone number. Use demo numbers: 9157992385'
                     : 'Invalid username. Use: om.somani'
                 );
             }
